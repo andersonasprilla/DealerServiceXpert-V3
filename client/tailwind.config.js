7c08d61fe8eaf15bb9ad287f2dd5e585f279482a
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-06-27 21:29:13 -0400

    initial commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
