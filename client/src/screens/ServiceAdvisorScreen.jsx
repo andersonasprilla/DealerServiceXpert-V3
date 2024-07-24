2c0a11d7c744b467635a141e58c0bd7451dd0a97
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-07-24 17:53:21 -0400

    fix route path version one

--- client/src/screens/ServiceAdvisorScreen.jsx
+++ client/src/screens/ServiceAdvisorScreen.jsx
@@ -2,29 +2,14 @@ import Layout from "../components/Layout/Layout"
 import Container from "../components/Container/Container"
 import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
-import { checkAuthStatus, getCustomers } from "../api/api"
+import { checkAuthStatus, getRepairOrders } from "../api/api"
 
-const formatCustomersData = (customersData) => {
-  return customersData.map(customer => {
-    const { hatNumber, repairOrder, createdAt, customerName, vehicle, contact, priority, status } = customer
-    return { 
-      Hat: hatNumber, 
-      repairOrder, 
-      Opened: createdAt, 
-      customerName, 
-      vehicle, 
-      contact, 
-      priority, 
-      status 
-    }
-  })
-}
 
 
 const ServiceAdvisorScreen = () => {
   const navigate = useNavigate()
   // State to store customer data
-  const [customers, setCustomers] = useState([])
+  const [repairOders, setRepairOrders] = useState([])
 
   useEffect(() => {
     const verifyAuth = async () => {
@@ -37,11 +22,9 @@ const ServiceAdvisorScreen = () => {
           return
         }
         // Fetch customer data
-        const customersData = await getCustomers()
-        // Format customer data
-        const formattedCustomers = formatCustomersData(customersData)
+        const repairOrderdata = await getRepairOrders()
         // Update state with formatted customer data
-        setCustomers(formattedCustomers)
+        setRepairOrders(repairOrderdata)
       } catch (error) {
         console.error('Authentication failed:', error)
         // Redirect to home on authentication failure
@@ -55,9 +38,9 @@ const ServiceAdvisorScreen = () => {
   return (
     <Layout>
       {/* Map over customers and render Container for each customer */}
-      {customers.map((customer, index) => (
-        <Container key={index} data={customer} />
-      ))}
+      {/* {repairOders.map((repairOrder, index) => (
+        <Container key={index} data={repairOrder} />
+      ))} */}
     </Layout>
   )
 }
