861c45188c4f5f0cfdb7a7f8337d31dffa99d8b7
anicabarrios <anicabarrios1@gmail.com>
2024-09-18 15:05:08 -0400

    created RO component

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Navbar from '../Navbar/Navbar'; 
 import Sidebar from '../Sidebar/Sidebar'; 
 import MobileSidebar from '../MobileSidebar/MobileSidebar'; 
-
+import OpenROs from '../OpenROs/OpenROs';
 // Layout component that wraps around the main content
 const Layout = ({ children }) => {
     const [sidebarOpen, setSidebarOpen] = useState(false); // State to control the visibility of the mobile sidebar
@@ -16,7 +16,10 @@ const Layout = ({ children }) => {
             {/* Main content area with padding for the sidebar */}
             <div className="lg:pl-72">
                 {/* Navbar with a button to toggle the mobile sidebar */}
+                
                 <Navbar setSidebarOpen={setSidebarOpen} />
+                
+                <OpenROs/>
                 <main className="py-10">
                     <div className="px-4 sm:px-6 lg:px-8">
                         {children} {/* Content passed as children */}
